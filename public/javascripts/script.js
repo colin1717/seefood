7683cd4595146043ae89c54393e4c50787744ee8
Colin Hancock <colin1717@mac.com>
2016-06-14 14:06:15 -0500

    started ajax

---
+++ public/javascripts/script.js
@@ -0,0 +1,22 @@
+$('#test').click(function(){
+  getMenuItems();
+})
+
+function getMenuItems(){
+  $.ajax({
+    url: '/menuitems',
+    method: 'GET',
+    dataType: 'JSON'
+  })
+    .done(function(data, textStatus){
+      console.log(data);
+      addMenuBox(data);
+    })
+    .fail(function(data, textStatus){
+      console.log("Error getting food items.  Status: " + textStatus);
+    })
+}
+
+function addMenuBox(foodItem) {
+  $('#sidebar-menu').prepend('<div class="menubox"><h3>'+ foodItem[0].name +'</h3></div>')
+}
