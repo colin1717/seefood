cff8d465bf0749888273964bb7c9c445eaf40b3b
Colin Hancock <colin1717@mac.com>
2016-06-14 11:25:19 -0500

    RESTful api working for menuItems

--- routes/menuItems.js
+++ routes/menuItems.js
@@ -26,4 +26,58 @@ router.post('/', function(req, res, next){
   });
 });
 
+//middleware
+router.use('/:menuItemId', function(req, res, next){
+  MenuItem.findOne({'_id': req.params.menuItemId }, function(err, menuItem){
+    if (err) {
+      res.status(500).send();
+    } else {
+      if (menuItem) {
+        res.menuItem = menuItem;
+        next();
+      } else {
+        res.status(404).send();
+      }
+    }
+  });
+});
+
+/* Get /menuItems/:menuItemId */
+router.get('/:menuItemId', function(req, res, next) {
+  res.json(res.menuItem);
+})
+
+/* PUT /menuItems/:menuItemId */
+router.put('/:menuItemId', function(req, res, next) {
+  MenuItem.findByIdAndUpdate(req.params.menuItemId, { $set: req.body }, function(err, menuItem){
+    if (err) {
+      res.status(500).send();
+    } else {
+      MenuItem.findOne({'_id': req.params.menuItemId }, function(err, menuItem){
+        if (err) {
+          res.status(500).send();
+        } else {
+          if (menuItem){
+            res.menuItem = menuItem;
+            res.json(res.menuItem);
+          } else {
+            res.status(404).send();
+          }
+        }
+      });
+    }
+  });
+});
+
+/* Delete /menuItem */
+router.delete('/:menuItemId', function(req, res, next){
+  MenuItem.remove({'_id': res.menuItem._id}, function(err){
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.status(204).send();
+    }
+  })
+});
+
 module.exports = router;
